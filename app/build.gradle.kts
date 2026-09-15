8977befe69248788e8a2fee09726f2fa5a0469e7
绫猫 <75155966+NEORUAA@users.noreply.github.com>
2026-09-16 03:12:11 +0800

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xposed.wetypehook"
         minSdk = 31
         targetSdk = 37
-        versionCode = 33
-        versionName = "1.27.1"
+        versionCode = 35
+        versionName = "1.28.0"
     }
 
     buildTypes {
